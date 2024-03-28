6158eb819d659c4b2dd4df02392c26db7484870e
M.Dhimas A <138362208+Pegasus7788@users.noreply.github.com>
2024-03-28 08:58:36 +0700

    Add files via upload
    menambahkan view latihan

---
+++ view-latihan.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $n1; ?>
+    Nilai 2 = <?= $n2; ?>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $n1 . " + " . $n2 . " = " . $hasil; ?>
+</body>
+</html>
