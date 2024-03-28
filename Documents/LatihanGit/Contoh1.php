011cc24640bdf00269f2f7c54fb8830ce37edf38
M.Dhimas A <138362208+Pegasus7788@users.noreply.github.com>
2024-03-28 08:55:59 +0700

    Add files via upload
    menambahkan contoh1

---
+++ Documents/LatihanGit/Contoh1.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+class Contoh1  extends Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Mohammad Dhimas Arifullah
+        Saya Tinggal di daerah Jakarta Pusat
+        Olahraga Yang saya sukai Bulutangkis";
+    }
+}
+?>
